6d07fa8fe798c564f0732b46377a5b1c75d545df
JDragon <jaeyonglee06@gmail.com>
2024-08-22 22:25:03 +0900

    junit test

--- src/main/java/com/sparta/springprepare/controller/HelloController.java
+++ src/main/java/com/sparta/springprepare/controller/HelloController.java
@@ -1,4 +1,4 @@
-package org.sparta.springprepare.controller;
+package com.sparta.springprepare.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
--- src/test/java/com/sparta/springprepare/SpringPrepareApplicationTests.java
+++ src/test/java/com/sparta/springprepare/SpringPrepareApplicationTests.java
@@ -1,4 +1,4 @@
-package org.sparta.springprepare;
+package com.sparta.springprepare;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
